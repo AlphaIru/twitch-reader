6dd31907811b76e6fe5bbebe575b9a8eba4cc942
AlphaIru <latioslatias2002@gmail.com>
2026-04-03 00:31:42 -0700

    at least the message recieve is working

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # twitch-reader
+
 The small application that would TTS the twitch chats.
+
+Note:
+Used Codeium and Gemini to write this project time to time.
+
+How to get Twitch API key:
+<https://dev.to/fedor-pasynkov/unlock-your-twitch-bots-potential-the-ultimate-oauth2-guide-22he#step-1-registering-your-application-on-the-twitch-developer-portalhttps://dev.to/fedor-pasynkov/unlock-your-twitch-bots-potential-the-ultimate-oauth2-guide-22he#step-1-registering-your-application-on-the-twitch-developer-portal>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+/*
+ * AlphaIru
+ * Twitch Reader
+ *
+ * This is a simple program that would read out
+ * Twitch chat messages to Open_JTalk in real-time.
+ * 
+ * main.rs
+ *
+ * This is the main entry point of the program and handles
+ * the main loop of the program.
+ *
+ * */
+
+mod twitch;
+
+use dotenvy::dotenv;
+use std::env;
+use tokio::sync::mpsc;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+    let username = env::var("TWITCH_USERNAME")
+        .expect("Error: .env file not found or TWITCH_USERNAME must be set");
+    // println!("Username: {}", username);
+
+    let oauth_token = env::var("TWITCH_OAUTH_TOKEN")
+        .expect("Error: .env file not found or TWITCH_OAUTH_TOKEN must be set");
+    // println!("Oauth Token: {}", oauth_token);
+
+    let (tx, mut rx) = mpsc::channel::<String>(32);
+
+    tokio::spawn(async move {
+        twitch::run_twitch_listener(username, oauth_token, tx).await;
+    });
+
+    println!("Twitch Reader is running!");
+
+    while let Some(msg) = rx.recv().await {
+        println!("Received message: {}", msg);
+    }
 }
+
